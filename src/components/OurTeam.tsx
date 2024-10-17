a42e191690bde57e15272a439bdd61e9fb8d78f1
LegendOP1098 <vaibhavgupta1098@gmail.com>
2024-10-17 21:03:02 +0530

    Updated team's idenfs

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OurTeam = () => {
   const teamMembers = [
-    { name: 'Ae', role: 'Founder & CEO', image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' },
+    { name: 'Ae', role: 'Program Director', image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' },
     { name: 'Be', role: 'Program Director', image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' },
     { name: 'Ce', role: 'Outreach Coordinator', image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' },
   ];
