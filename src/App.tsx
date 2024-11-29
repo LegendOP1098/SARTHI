ffe7bb6fc79bb72fe0a627f24d251236efc3a41d
Pikachu-345 <vishal2patel4@gmail.com>
2024-11-30 02:17:01 +0530

    checkin to proj

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,9 +10,7 @@ import GetInvolved from './pages/GetInvolved';
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-gray-50">
         <Header />
-        <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/our-work" element={<OurWork />} />
@@ -21,9 +18,7 @@ function App() {
             <Route path="/our-team" element={<OurTeam />} />
             <Route path="/get-involved" element={<GetInvolved />} />
           </Routes>
-        </main>
         <Footer />
-      </div>
     </Router>
   );
 }
